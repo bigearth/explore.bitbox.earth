079a675a3098179f535c541ce1e8de8366465e98
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-05-06 07:55:57 -0700

    Styles

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -106,7 +106,7 @@ class Homepage extends Component {
       });
     }
     return (
-      <div>
+      <div className='Homepage'>
         <div className="splash-container">
           <div className="splash">
             <h1 className="splash-head">EXPLORE</h1>
@@ -123,23 +123,23 @@ class Homepage extends Component {
         </div>
         <div className="content-wrapper">
           <div className="content features">
-              <div className="pure-g">
-                  <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-2">
-                    <p className="header-icon"><i className="fa fa-cubes" /></p>
-                    <h2 className="content-head">
-                      Latest Blocks
-                    </h2>
-                    <ul>
-                      {blocks}
-                    </ul>
-                  </div>
-                  <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-2">
-                    <p className="header-icon"><i className="fa fa-exchange-alt" /></p>
-                    <h2 className="content-head">
-                      Latest Transactions
-                    </h2>
-                  </div>
+            <div className="pure-g">
+              <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-2">
+                <p className="header-icon"><i className="fa fa-cubes" /></p>
+                <h2 className="content-head">
+                  Latest Blocks
+                </h2>
+                <ul>
+                  {blocks}
+                </ul>
               </div>
+              <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-2">
+                <p className="header-icon"><i className="fa fa-exchange-alt" /></p>
+                <h2 className="content-head">
+                  Latest Transactions
+                </h2>
+              </div>
+            </div>
           </div>
           <div className="footer l-box is-center">
             Created w/ ❤️ on EARTH by <a href='https://twitter.com/bitboxearth'>@bitboxearth</a>. Donations <strong>bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c</strong>

ca5cfa01e7b92c79325a5721b68a213e9e87765e
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-05-06 16:25:54 -0700

    vin/vout

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -28,15 +28,14 @@ class Transaction extends Component {
       this.setState({
         blockhash: result.blockhash,
         blockheight: result.blockheight,
-        blocktime: result.blocktime,
         confirmations: result.confirmations,
         isCoinBase: result.isCoinBase,
-        locktime: result.locktime,
         size: result.size,
         time: result.time,
         txid: result.txid,
         valueOut: result.valueOut,
-        version: result.version
+        vin: result.vin,
+        vout: result.vout
       });
     }, (err) => { console.log(err);
     });
@@ -46,6 +45,40 @@ class Transaction extends Component {
     let coinbase;
     if(this.state.isCoinBase) {
       coinbase = <p><i className="fab fa-bitcoin" /> coinbase</p>
+    }
+
+    let vin = [];
+    if(this.state.vin) {
+      this.state.vin.forEach((v, ind) => {
+        if(v.coinbase) {
+          vin.push(
+            <li key={ind}>Coinbase</li>
+          );
+        } else {
+          vin.push(
+            <li key={ind}>
+              <Link
+                to={`/address/${this.props.bitbox.Address.toCashAddress(v.addr)}`}>
+                {this.props.bitbox.Address.toCashAddress(v.addr)}
+              </Link>
+            </li>
+          );
+        }
+      });
+    }
+
+    let vout = [];
+    if(this.state.vout) {
+      this.state.vout.forEach((v, ind) => {
+        vout.push(
+          <li key={ind}>
+            <Link
+              to={`/address/${this.props.bitbox.Address.toCashAddress(v.scriptPubKey.addresses[0])}`}>
+              {this.props.bitbox.Address.toCashAddress(v.scriptPubKey.addresses[0])}
+            </Link>
+          </li>
+        );
+      });
     }
 
     return (
@@ -63,6 +96,20 @@ class Transaction extends Component {
             <p><i className="far fa-calendar-alt" /> Time: {moment.unix(this.state.time).format('MMMM Do YYYY, h:mm:ss a')}</p>
             <p><i className="far fa-id-card" /> txid: {this.state.txid}</p>
             <p><i className="fab fa-bitcoin" /> valueOut: {this.state.valueOut}</p>
+          </div>
+        </div>
+        <div className="pure-g">
+          <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-2">
+            <h3 className='content-subhead'>Inputs</h3>
+            <ul>
+              {vin}
+            </ul>
+          </div>
+          <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-2">
+            <h3 className='content-subhead'>Outputs</h3>
+            <ul>
+              {vout}
+            </ul>
           </div>
         </div>
       </div>

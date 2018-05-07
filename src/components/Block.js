4d5d5a91cfde105e89a097c19f79d380699d5709
Eric Lima <ericjlima@gmail.com>
2018-05-07 13:12:13 -0400

    trying to sync but more pulls to do

--- src/components/Block.js
+++ src/components/Block.js
@@ -5,6 +5,7 @@ import {
   withRouter
 } from 'react-router-dom';
 import Slider from 'react-slick';
+import ReactPaginate from 'react-paginate';
 
 import "../styles/homepage.scss";
 
@@ -126,6 +127,17 @@ class Block extends Component {
             <p><i className="fas fa-wrench" /> Miner: <a href={this.state.poolInfo.url}>{this.state.poolInfo.poolName}</a></p>
           </div>
         </div>
+        <ReactPaginate previousLabel={"previous"}
+                       nextLabel={"next"}
+                       breakLabel={<a href="">...</a>}
+                       breakClassName={"break-me"}
+                       pageCount={this.state.pageCount}
+                       marginPagesDisplayed={2}
+                       pageRangeDisplayed={5}
+                       onPageChange={this.handlePageClick}
+                       containerClassName={"pagination"}
+                       subContainerClassName={"pages pagination"}
+                       activeClassName={"active"} />
       </div>
     );
   }
